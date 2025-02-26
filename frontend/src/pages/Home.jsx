4ddb4cbc9c4ff117d7bc5bd52cebf51075799d1a
maheshkokate07 <kokatemahesh2004@gmail.com>
2025-02-26 19:23:41 +0530

    Upload video functionality implemented

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,7 +6,7 @@ import VideoCard from "../components/VideoCard";
 function Home() {
     const dispatch = useDispatch();
     const { videos = [] } = useSelector(state => state.videos.videos);
-    const {loading, error} = useSelector(state => state.videos);
+    const { loading, error } = useSelector(state => state.videos);
 
     useEffect(() => {
         dispatch(fetchVideos());
@@ -14,6 +14,9 @@ function Home() {
 
     return (
         <div className="px-4 py-2 w-full h-[calc(100vh-57px)] overflow-auto">
+            {
+                loading && <div className="text-center text-md mt-3 mb-5 w-full">Fetching latest videos...</div>
+            }
             <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
                 {videos?.length > 0 ? (
                     videos?.map((video) => <VideoCard key={video._id} video={video} />)
