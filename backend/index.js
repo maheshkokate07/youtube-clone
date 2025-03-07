194d365bc9ec860e55e1a5f08eaa752f531450e3
maheshkokate07 <kokatemahesh2004@gmail.com>
2025-03-07 22:30:20 +0530

    Added

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,10 @@ import authRoutes from "./routes/authRoutes.js";
 import channelRoutes from "./routes/channelRoutes.js";
 import videoRoutes from "./routes/videoRoutes.js";
 import commentRoutes from "./routes/commentRoutes.js";
-import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
