5acc360f6295479e5542af91d7a75fe97babee80
maheshkokate07 <kokatemahesh2004@gmail.com>
2025-03-07 22:24:20 +0530

    Added

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URL, {
