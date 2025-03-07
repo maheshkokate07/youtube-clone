5cab269bda875c274f0219109269614893eda04f
maheshkokate07 <kokatemahesh2004@gmail.com>
2025-03-07 22:32:10 +0530

    Added

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/authRoutes.js";
 import channelRoutes from "./routes/channelRoutes.js";
 import videoRoutes from "./routes/videoRoutes.js";
 import commentRoutes from "./routes/commentRoutes.js";
+import path from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
